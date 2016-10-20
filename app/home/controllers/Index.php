b024aab23aa8f58a1d76640248da4be9fff1282f
董坤鸿 <249529979@qq.com>
2016-10-20 21:10:05 +0800

    创建测试控制器

---
+++ app/home/controllers/Index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * Name:
+ * Index.php 首页控制器
+ * User: ken
+ * Date: 2016/10/20
+ * Time: 下午9:07
+ */
+
+namespace app\home\controllers;
+class Index {
+
+    /**
+     * 显示首页
+     */
+    public function show(){
+        echo "我的框架";
+    }
+
+}
